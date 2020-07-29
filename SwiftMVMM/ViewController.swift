1d6ac2e9f1202d6e10b7409e5a8ea834087ef5da
Kemal TUNÇ <kemaltunc191@gmail.com>
2020-07-29 20:05:01 +0300

    Initial Commit

---
+++ SwiftMVMM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftMVMM
+//
+//  Created by Kemal TUNÇ on 29.07.2020.
+//  Copyright © 2020 Kemal TUNÇ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
